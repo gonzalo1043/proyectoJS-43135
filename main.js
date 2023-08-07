f2f1300e763f4e46aa231585a53f41c4422d7453
gonzalo martinez ciresa <gonzalo1043@gmail.com>
2023-08-07 14:50:51 -0300

    Proyecto finalizado. Contiene funciones asincornicas, eventos, promesas, clases, DOM, localStorage, funciones de orden superior, operadores avanzados, librerias

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+
 let porcentaje = 0.90
-
-
 function carrito () {
+
+    // Vinilos para comprar Vinilos para comprarVinilos para comprarVinilos para comprar
+    // Vinilos para comprar Vinilos para comprar Vinilos para comprar Vinilos para comprar
     const vinilos = [
         {
         id: 1,
@@ -69,47 +71,279 @@ function carrito () {
     ]
 
 
-    const carrito = [] 
-    let botonesComprar = document.getElementsByClassName ('btn')
+    
+    //CAMBIO DE PRECIO PARA VINILOS CON DESCUENTO
+    let vinilosDescuentoPrecio = vinilos.filter((d) => d.descuento.includes('SI'))
+    for (const vinilo of vinilosDescuentoPrecio) {
+        vinilo.precio = Math.round (vinilo.precio * porcentaje)
+    }
+    
+    // SWEET ALERT VINILOS CON DESCUENTO.
+    // SWEET ALERT VINILOS CON DESCUENTO.
+    let botonDescuento = document.getElementById('descuento')
+    botonDescuento.onclick = () => {
+        let vinilosDescuento = vinilos.filter((v) => v.descuento.includes('SI'));
 
+        let vinilosPrueba = []
+
+        vinilosDescuento.forEach ((vinilo) => {
+            vinilosPrueba.push(`${vinilo.nombre} , precio: USD$ ${vinilo.precio}`)
+        })
+
+        swal ({
+            title: 'VINILOS CON DESCUENTO',
+            text: `LOS VINILOS CON DESCUENTO SON: \n ${vinilosPrueba.join('\n')} `
+        })
+    }
+
+
+
+    // BOTON COMPRAR DE CADA VINILO
+    const carrito = [] 
+    let botonesComprar = document.getElementsByClassName ('botonComprar')
     for (const boton of botonesComprar) {
         boton.onclick = (e) => {
             let vinilosSeleccionados = vinilos.find ((vinilo) => vinilo.id === parseInt(e.target.id))
-            alert (`Se agrego ${vinilosSeleccionados.nombre}`)
-            
-            vinilosSeleccionados.descuento == 'SI' ? vinilosSeleccionados.precio =(Math.round(vinilosSeleccionados.precio * porcentaje)) : vinilosSeleccionados.precio = vinilosSeleccionados.precio
 
-            vinilosSeleccionados.descuento == 'SI' ? alert (`Debido a un descuento el precio de este vinilo queda en USD$${vinilosSeleccionados.precio}`) : alert (`El precio de este vinilo es de USD$${vinilosSeleccionados.precio}`)
-            carrito.push(vinilosSeleccionados)
+            vinilosSeleccionados.descuento == 'SI' ? 
+            swal({
+                title: `¿Desea agregar el vinilo al carrito?`,
+                text: `Por un descuento el vinilo ${vinilosSeleccionados.nombre} tiene un precio de: USD$ ${vinilosSeleccionados.precio}`,
+                    buttons: ['Cancelar', 'Aceptar'],
+            }).then((respuesta) => {
+                if(respuesta){
+                    carrito.push(vinilosSeleccionados)
+                        console.log(carrito)
+                }
+            }) : swal({
+                title: `¿Desea agregar el vinilo al carrito?`,
+                text: `${vinilosSeleccionados.nombre}, precio: ${vinilosSeleccionados.precio}`,
+                buttons: ['Cancelar', 'Aceptar'],
+            }).then((respuesta) => {
+                if(respuesta) {
+                    carrito.push(vinilosSeleccionados)
+                    console.log(carrito)
+                }
+            })
         }
     }
-            
-    let botonFinalizar = document.getElementById('finalizarCompra')
-    botonFinalizar.onclick = () => {
-        console.log(`Se cargo el carrito con ${carrito.length} unidades de productos`)
+
+
+    //BOTON GUARDAR COMPRA
+    //BOTON GUARDAR COMPRA
+    let botonGuardarCompra = document.getElementById('guardarCompra')
+    botonGuardarCompra.onclick = () => {
         localStorage.setItem ('carrito', JSON.stringify(carrito))
         
         sumaTotal = 0
         for (const vinilos of carrito) {
             sumaTotal += vinilos.precio
         }
-        alert (`La suma total de su compra es de: USD$${sumaTotal}`)
+        swal({
+            title: 'Se ha guardado su compra',
+            icon: 'success',
+            text: `Ir al carrito para continuar`,
+            buttons: ['Cancelar', 'Aceptar']
+        })
         localStorage.setItem ('sumaTotal', sumaTotal)
         console.log (carrito)
     }
 
+    //BOTON CARRITO
+    //BOTON CARRITO
     let botonMostrarCarrito = document.getElementById ('mostrarCarrito')
     botonMostrarCarrito.onclick = () => {
+
+        function tiempoDeEspera() {
+            return new Promise((resolve, reject) => {
+                setTimeout(()=>{
+                    if (carritoLocalStorage){
+                        resolve(finalizarCompra)
+                    } else {
+                        reject('No tiene ningun vinilo en el carrito')
+                    }
+                }, 1000)
+                })
+        }
         const carritoLocalStorage = JSON.parse(localStorage.getItem('carrito'))
-
-        const nombreVinilos = carritoLocalStorage.map ((vinilo) => vinilo.nombre )
         
-        const precioFinalLocalStorage = localStorage.getItem('sumaTotal')
+        function finalizarCompra () {
+            const nombreVinilos = carritoLocalStorage.map ((vinilo) => vinilo.nombre).join('\n')
+            const precioFinalLocalStorage = localStorage.getItem('sumaTotal')
 
-
-        alert (`Los vinilos en el carrito son: ${nombreVinilos} y su precio es de: USD$ ${precioFinalLocalStorage}`)
+            swal({
+                title: 'CARRITO',
+                text: `Los vinilos en el carrito son: \n ${nombreVinilos} \n su precio es de: USD$ ${precioFinalLocalStorage}`,
+                buttons: ['Cancelar', 'Finzalizar compra']
+            }).then((resultado) => {
+                if (resultado) {
+                    swal({
+                        title: 'Su compra ha sido realizada con exito.',
+                        icon: 'success',
+                    }).then((resultado) => {
+                        if (resultado) {
+                            Toastify({
+                                text: 'Gracias por comprar en nuestro sitio web :)',
+                                style: {
+                                    backgroud: '#FFC600'
+                                },
+                                duration: 3000,
+                            }).showToast();
+                        }
+                    })
+                localStorage.clear();
+        }
+        })
+        }
+        
+        let spinner = document.getElementById('spinner')
+        spinner.style.display = 'grid'
+        
+        tiempoDeEspera().then(() => {
+            spinner.style.display = 'none'
+            finalizarCompra()
+        }).catch ((error) => {
+            spinner.style.display = 'none'
+            swal({
+                title: 'CARRITO',
+                text: error
+            })
+        })
     }
+
+
+    // PROXIMOS VINILOS
+    // PROXIMOS VINILOS
     
+    function proximosVinilos() {
+        
+        const reservados = []
+        
+        let boton = document.getElementById ('proximosVinilos')
+        boton.onclick = () => {
+            
+            let spinner = document.getElementById('spinner2')
+            spinner.style.display = 'grid'
+        
+            async function vinilosProximos () {
+            const proximos = await fetch ('./data.json')
+            const vinilosProximosArray = await proximos.json()
+            
+            for (const vinilos of vinilosProximosArray) {
+                let contenedor = document.createElement('div')
+                contenedor.innerHTML = `
+                <div class="col">
+                    <div class="card" style="width: 18rem;">
+                        <img src= "${vinilos.img}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">${vinilos.nombre} - ${vinilos.artista} </h5>
+                            <p class="precio">USD$ ${vinilos.precio} </p>
+                            <button id = ${vinilos.id} class="btn btn-primary botonComprar botonReservar">RESERVAR</button class="">
+                        </div>
+                    </div>
+                </div>
+                `
+                document.querySelector ('#divProximosVinilos').appendChild(contenedor)
+            }
+            
+            //BOTON RESERVAR
+            let botonesReservar = document.getElementsByClassName('botonReservar')
+            for (const reservarbtn of botonesReservar) {
+                reservarbtn.onclick = (e) => {
+                    let vinilosReservados = vinilosProximosArray.find ((vinilo) => vinilo.id === parseInt(e.target.id))
+                    swal({
+                        title: `¿Desea reservar este vinilo?`,
+                        text: `${vinilosReservados.nombre}, precio: USD$ ${vinilosReservados.precio}`,
+                        buttons: ['Cancelar', 'Aceptar'],
+                    }).then((respuesta) => {
+                        if(respuesta) {
+                            reservados.push(vinilosReservados)
+                            console.log(reservados)
+                        }
+                    })
+                }
+            }
+            
+            //CONFIRMAR RESERVA
+            let botonConfirmarReserva = document.createElement('button')
+            botonConfirmarReserva.className = 'btn btn-primary'
+            botonConfirmarReserva.innerText = 'CONFIRMAR RESERVA'
+            document.querySelector('.botonConfirmarReserva').appendChild(botonConfirmarReserva)
+
+            botonConfirmarReserva.onclick = () => {
+                const nombreR = reservados.map ((vinilos) => vinilos.nombre).join('\n')
+                swal({
+                    title: 'Se ha guardado su reserva',
+                    icon: 'success',
+                }).then ((respuesta) => {
+                    if(respuesta) {
+                        swal({
+                            title: 'RESERVAS',
+                            text: `Los vinilos reservados son: \n ${nombreR}`,
+                            buttons: ['Cancelar', 'Confirmar reserva'],
+                        }).then ((respuesta) => {
+                            if(respuesta) {
+                                
+                                localStorage.setItem ('reservas', JSON.stringify(reservados))
+                                
+                                Toastify({
+                                    text: 'Vinilos reservados',
+                                style: {
+                                    backgroud: '#FFC600'
+                                },
+                                duration: 4000,
+                                }).showToast()
+                            }
+                        })
+                    }
+                })
+                }
+            //VER RESERVA
+                let botonVerReserva = document.createElement('button')
+                botonVerReserva.className = 'btn btn-primary'
+                botonVerReserva.innerText = 'VER RESERVA'
+                document.querySelector('.verVinilosReservados').appendChild(botonVerReserva)
+                
+                botonVerReserva.onclick = () => {
+
+                function tiempoDeEsperaReserva() {
+                    return new Promise((resolve, reject) => {
+                        setTimeout(()=>{
+                            if (reservadosLocalStorage){
+                                resolve(verReserva)
+                            } else {
+                                reject('No tiene ningun vinilo reservado')
+                            }
+                        }, 500)
+                        })
+                }
+                const reservadosLocalStorage = JSON.parse(localStorage.getItem('reservas'))
+                
+                function verReserva () {
+                const nombreReservados = reservadosLocalStorage.map ((vinilos) => vinilos.nombre).join('\n')
+                swal({
+                    title: 'RESERVAS',
+                    text: `Los vinilos reservados son: \n ${nombreReservados}`
+                })
+                }
+
+                tiempoDeEsperaReserva().then(() => {
+                    verReserva()
+                }).catch ((error) => {
+                    swal({
+                        title: 'RESERVAS',
+                        text: error
+                    })
+                })
+            }
+            spinner.style.display='none'
+            
+        }vinilosProximos()
+        }
+        }
+    proximosVinilos()
+
+    //ELEGIR VINILOS NUEVOS
     function ElegirvinilosNuevos () {
         let submit = document.getElementById('submit');
         let nuevoVinilo = document.getElementById('nombreViniloNuevo')
@@ -122,36 +356,74 @@ function carrito () {
         }
         let vinilosNuevos = []
         
+
+        //BOTON AGREGAR
         submit.onclick = () => {
             vinilosNuevos.push(new VinilosNuevos ((nuevoVinilo.value), (nuevoArtista.value)))
-            localStorage.setItem ('encargo', JSON.stringify(vinilosNuevos))
 
+            swal({
+                icon: 'success',
+            })
         }
-        let botonConfirmar = document.getElementById('confirmar')
         
+        //BOTON CONFIRMAR
+        let botonConfirmar = document.getElementById('confirmar')
         botonConfirmar.onclick = () => {
-            for (const vinilo of vinilosNuevos) {
-                alert (`A encargado el vinilo ${vinilo.nombre} del artista ${vinilo.artista}`)
-        }
+
+        localStorage.setItem ('encargo', JSON.stringify(vinilosNuevos))
+        
+        let vinilosConfirmados = []
+
+        vinilosNuevos.forEach ((vinilos) => {
+            vinilosConfirmados.push(`${vinilos.nombre} del artista ${vinilos.artista}`)
+        })
+
+        // let vinilosConfirmadosNombre = vinilosNuevos.map((vinilos) => vinilos.nombre).join('\n')
+
+        swal ({
+            title: 'VINILOS ENCARGADOS',
+            text: `Encargo los vinilos ${vinilosConfirmados.join('\n')}`,
+            icon: 'success',
+        })
         }
 
+        // BOTON ENCARGADOS 
         let botonEncargo = document.getElementById('encargo')
-
         botonEncargo.onclick = () => {
             const encargoLocalStorage = JSON.parse(localStorage.getItem('encargo'))
 
+            function tiempoDeEsperaEncargos () {
+                return new Promise ((resolve, reject) => {
+                    setTimeout(() => {
+                        if (encargoLocalStorage) {
+                            resolve(verEncargados)
+                        } else {
+                            reject('No ha encargado ningun vinilo')
+                        }
+                    }, 500);
+                })
+            }
+            function verEncargados() {            
+            
             for (const encargo of encargoLocalStorage) {
-                alert (`Los vinilos encargados son ${encargo.nombre} del artista ${encargo.artista}`)
                 let encargados = document.createElement('div')
                 encargados.innerHTML = `<h3> A encargado el vinilo ${encargo.nombre} del artista ${encargo.artista} </h3>`
-                document.body.append(encargados)
+                document.querySelector('.vinilosEncargados').append(encargados)
             }
+            }
+            tiempoDeEsperaEncargos().then(() => {
+                verEncargados()
+            }).catch((error) => {
+                swal ({ 
+                    title: 'ENCARGADOS',
+                    text: error
+                })
+            })
         }
     }
     ElegirvinilosNuevos()
 }
 carrito()
-
 
 
 
